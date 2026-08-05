85dbd5166ce8caa99cdd3ed19b4b70ef351d3509
jvdalmeida <jvdalmeida000@gmail.com>
2026-08-04 22:06:22 -0300

    Update Exercicio1.php

--- SW/Atividade1/Exercicio1.php
+++ SW/Atividade1/Exercicio1.php
@@ -1,1 +1,26 @@
 
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+    <form method="post">
+        Numero: <input type="number" name="numero" required>
+        <input type="submit" value="Verificar">
+    </form>
+
+    <?php
+    if (isset($_POST['numero'])) {
+        $numero = $_POST['numero'];
+
+        if ($numero % 2 == 0) {
+            echo "O numero $numero é par";
+        } else {
+            echo "O numero $numero é impar";
+        }
+    }
+    ?>
+
+</body>
+</html>
